05c919f5f476b36fc736079443ab2a58ad827b08
Rohan Rathore <75261923+Rohan-Rathore@users.noreply.github.com>
2023-03-06 12:25:31 +0530

    Create dataRead.js
    Reading data from deployed smart contract mycontract.sol

---
+++ dataRead.js
@@ -0,0 +1,16 @@
+var Web3 = require('web3');
+var Contract = require('web3-eth-contract');
+
+Contract.setProvider(new Web3.providers.HttpProvider('http://localhost:8545'));
+
+var myContract = new Contract(<ABI>,
+"0xb3f36458FFc0C686DB4f2FF6002a55bFD85C03C8",
+                              
+{
+from: "0xd84a0607843b28c3f468857f82f784d9ff743bf8",
+gasPrice: "20000000000"
+}
+
+);
+
+myContract.methods.readdata().call().then(function (output) { console.log(output) });
